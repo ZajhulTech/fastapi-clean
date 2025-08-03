64268377d6b1ab7be5d722c3d3eed41fc37261bb
Saul Dueñas B. <saul.duenas@hotmail.com>
2025-08-02 20:19:15 -0600

    add security

--- app/webapi/main.py
+++ app/webapi/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.infra.api.exception_handlers import http_exception_handler, unhandled_exception_handler
+from fastapi.exceptions import HTTPException
 from app.webapi.routers import user_router, customer_router
 
 
@@ -6,6 +8,10 @@ app = FastAPI(
     title="My FastAPI App",
     version="1.0.0"
 )
+
+# Registrar handlers globales
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(Exception, unhandled_exception_handler)
 
 app.include_router(user_router.router)
 app.include_router(customer_router.router)
